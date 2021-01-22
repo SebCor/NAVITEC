edafb8b262b6aa58bb4b1fdbc8639b7999c29a8d
Sebastian <Sebastian@DESKTOP-PHP5KCJ>
2021-01-21 22:06:49 -0600

    Funcionalidad de creacion de Cartas completa

--- navtec/src/navtec/Navtec.java
+++ navtec/src/navtec/Navtec.java
@@ -12,6 +12,8 @@ public class Navtec {
     public static void main(String[] args) {
        System.out.print("verdaa");
        System.out.print("SIIIII");
+       System.out.print("NOOOO");
+       System.out.print("SIIIII");
     }
     
 }
