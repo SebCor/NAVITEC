95d63d3acf737c81f5f6677e7c4f63eef2fcf241
Sebastian <Sebastian@DESKTOP-PHP5KCJ>
2021-01-21 22:09:27 -0600

    Funcionalidad de creacion de Cartas completa

---
+++ navtec/src/navtec/Carta.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package navtec;
+
+/**
+ * 
+ * @author Sammy Guergachi <sguergachi at gmail.com>
+ */
+public class Carta {
+    
+    
+    
+        public static void main(String[] args) {
+       System.out.print(" Error de capa 8 verdaaa");
+      
+    }
+}
