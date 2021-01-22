b19f85f772acd5dbd2ebf611ca7eaeee95f083bf
Sebastian <Sebastian@DESKTOP-PHP5KCJ>
2021-01-21 22:14:27 -0600

    Funcionalidad de creacion de Cartas completa

--- navtec/src/navtec/Carta.java
+++ navtec/src/navtec/Carta.java
@@ -7,6 +7,12 @@ public class Carta {
     
         public static void main(String[] args) {
        System.out.print(" Error de capa 8 verdaaa");
-      
+
+       System.out.print("SIIIII");
+       System.out.print("NOOOO");
+       System.out.print("Varas mae");
+       System.out.print("SIIIII");
     }
+      
+    
 }
