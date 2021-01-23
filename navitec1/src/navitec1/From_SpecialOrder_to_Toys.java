71c67de484ab9b571a59341f20de2e8981c6907c
Maria Laura <Maria Laura@DESKTOP-BC35MAB>
2021-01-23 09:23:11 -0600

    Corresponde a la función que pasa los juguetes de orden especial al catálogo

---
+++ navitec1/src/navitec1/From_SpecialOrder_to_Toys.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package navitec1;
+
+/**
+ *
+ * @author Maria Laura
+ */
+public class From_SpecialOrder_to_Toys {
+     System.out.println('The toy  is now in the table Toys');
+
+    
+}
