14d2a253296e22538139bc48560b89b22eaab3e1
Maria Laura <Maria Laura@DESKTOP-BC35MAB>
2021-01-23 09:14:13 -0600

    Corresponde a los reportes de niñxs

---
+++ navitec1/src/navitec1/Kids_by_Country_reports.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package navitec1;
+
+/**
+ *
+ * @author Maria Laura
+ */
+public class Kids_by_Country_reports {
+     System.out.println('behaviour reports');
+
+    
+}
