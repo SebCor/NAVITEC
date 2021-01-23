2df7e5978902c8f2598e99421e9be15bfef54417
Maria Laura <Maria Laura@DESKTOP-BC35MAB>
2021-01-23 09:20:16 -0600

    Corresponde a los reportes de niñxs

---
+++ navitec1/src/navitec1/Processsed_letter.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package navitec1;
+
+/**
+ *
+ * @author Maria Laura
+ */
+public class Processsed_letter {
+     System.out.println('behaviour reports');
+
+}
