3f14fb0f9c0706b699edd22d21bfa9a75e9ffd6c
Maria Laura <Maria Laura@DESKTOP-BC35MAB>
2021-01-23 09:25:46 -0600

    CRUD Wish list

---
+++ navitec1/src/navitec1/Wish_List_CRUD.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package navitec1;
+
+/**
+ *
+ * @author Maria Laura
+ */
+public class Wish_List_CRUD {
+     System.out.println('behaviour reports');
+
+}
