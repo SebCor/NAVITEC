0d1a3568b90973417dd75a7865a6299fe00a3acb
Maria Laura <Maria Laura@DESKTOP-BC35MAB>
2021-01-23 09:10:53 -0600

    Corresponde a los reportes de juguetes

---
+++ navitec1/src/navitec1/Toys_by_Country_Report.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package navitec1;
+
+/**
+ *
+ * @author Maria Laura
+ */
+public class Toys_by_Country_Report {
+     System.out.println('behaviour reports');
+
+    
+}
